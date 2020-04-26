ceb5b15c9a7eda525ef38ca0c912e1491c0a5dd2
Inocêncio <inocencio.cardoso@iatec.com>
2020-04-26 09:12:53 -0300

    added header and Tabs files

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+      <Container>
+          <top>
+              <Logo source={} />
+              <Title>Inocencio</Title>
+          </top>
+    </Container>
+  );
+};

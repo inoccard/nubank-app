e8b4f4d968c1c9e673d3cafe0a17f89e8e60e67b
Inocêncio <inocencio.cardoso@iatec.com>
2020-04-26 16:06:27 -0300

    adicionado content para exiir saldo disponível

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 import Header from '~/components/header';
 import Tabs from '~/components/tabs';
 
@@ -8,6 +9,25 @@ export default function Main() {
   return (
     <Container >
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666"/>
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description> R$ 109,889,45</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>Transferência de R$ 20,00 recebida de IC SD</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );

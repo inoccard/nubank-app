03430a59ad149a7a65c7320430b5154a8e1704f6
Inocêncio <inocencio.cardoso@iatec.com>
2020-05-30 02:03:27 -0300

    adicionado animações

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import { Animated } from 'react-native';
 
 import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 import Header from '~/components/header';
@@ -8,29 +9,82 @@ import Tabs from '~/components/tabs';
 import Menu from '~/components/menu';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+        translationY: translateY
+      }
+    }
+    ],
+    {
+      useNativeDriver: true
+    }
+)
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false
+      const { translationY } = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 100)
+        opened = true;
+      else {
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+        offset = 0;
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
-    <Container >
+    <Container>
       <Header />
-      <Menu/>
+      <Menu translateY={translateY} />
       <Content>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666"/>
-          </CardHeader>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}>
+          <Card style={
+            {
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0 , 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp'
+              }),
 
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description> R$ 109,889,45</Description>
-          </CardContent>
+            }
+          }>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666"/>
+            </CardHeader>
 
-          <CardFooter>
-            <Annotation>Transferência de R$ 20,00 recebida de IC SD</Annotation>
-          </CardFooter>
-        </Card>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description> R$ 109,889,45</Description>
+            </CardContent>
+
+            <CardFooter>
+              <Annotation>Transferência de R$ 20,00 recebida de IC SD</Annotation>
+            </CardFooter>
+            </Card>
+          </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 };
